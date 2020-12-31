cd278fd26bdab265b5fa5eb143e8d84e99472468
iftiaz <iftiaz.ahmed@yahoo.com>
2020-12-31 20:41:50 +0600

    hidden layers added, hyperparameters tuned

--- diabetes.py
+++ diabetes.py
@@ -10,6 +10,11 @@ import torch.optim as optim  # For all Optimization algorithms, SGD, Adam, etc.
 import torchvision
 import torchvision.transforms as transforms  # Transformations we can perform on our dataset
 from pandas import io
+import matplotlib
+import matplotlib.pyplot as plt
+import time
+
+start_time = time.time()
 
 # from skimage import io
 from torch.utils.data import (
@@ -23,12 +28,20 @@ import torch.nn as nn  # All neural network modules, nn.Linear, nn.Conv2d, Batch
 class NN(nn.Module):
     def __init__(self, input_size, num_classes):
         super(NN, self).__init__()
-        self.fc1 = nn.Linear(input_size, 10)
-        self.fc2 = nn.Linear(10, num_classes)
+        self.fc1 = nn.Linear(input_size, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, 16)
+        self.fc4 = nn.Linear(16, 16)
+        self.fc5 = nn.Linear(16, 8)
+        self.fc6 = nn.Linear(8, num_classes)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = self.fc6(x)
         return x
 
 
@@ -54,9 +67,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Hyperparameters
 num_classes = 2
-learning_rate = 0.01
-batch_size = 10
-num_epochs = 20
+learning_rate = 0.001
+batch_size = 5
+num_epochs = 1000
 input_size = 8
 
 # Load Data
@@ -77,6 +90,9 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 print(len(train_set))
 print(len(test_set))
+lss = []
+min_loss_found = 100
+min_loss_found_at_epoch = 0
 # Train Network
 for epoch in range(num_epochs):
     losses = []
@@ -98,6 +114,11 @@ for epoch in range(num_epochs):
 
         # gradient descent or adam step
         optimizer.step()
+
+    lss.append(sum(losses) / len(losses))
+    if sum(losses) / len(losses) < min_loss_found:
+        min_loss_found = sum(losses) / len(losses)
+        min_loss_found_at_epoch = epoch
 
     print(f"Cost at epoch {epoch} is {sum(losses) / len(losses)}")
 
@@ -130,3 +151,10 @@ check_accuracy(train_loader, model)
 
 print("Checking accuracy on Test Set")
 check_accuracy(test_loader, model)
+
+print(min_loss_found_at_epoch, min_loss_found)
+print("Execution time: %s seconds" % (time.time() - start_time))
+fig, ax = plt.subplots()
+ax.plot(range(num_epochs), lss)
+ax.set(xlabel='Epochs', ylabel='Loss')
+plt.show()
